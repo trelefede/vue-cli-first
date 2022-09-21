e04e09bf3625c3b12101fd53423c5ea81f872f83
TreleaniFederico <treleani.federico@gmail.com>
2022-09-21 16:29:31 +0200

    add new component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppTitle msg="La mia prima app con Vue CLI!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppTitle,
+
   }
 }
 </script>
